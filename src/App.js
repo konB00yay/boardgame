adc9b9dec9f8d8cf066fe659cd976dd3f0d49942
Konnor <kbeaulier@wisc.edu>
2020-04-24 22:17:09 -0700

    Add images and grid view

--- src/App.js
+++ src/App.js
@@ -1,56 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div class="App">
+      <div class="row">
+        <div class="column">
+          <img src={require("./spaces/space_09.png")} alt-="space 9" />
+          <img src={require("./spaces/space_08.png")} alt-="space 8" />
+          <img src={require("./spaces/space_07_gym_1.png")} alt-="space 7" />
+          <img src={require("./spaces/space_06.png")} alt-="space 6" />
+          <img src={require("./spaces/space_05.png")} alt-="space 5" />
+          <img src={require("./spaces/space_04.png")} alt-="space 4" />
+          <img src={require("./spaces/space_03.png")} alt-="space 3" />
+          <img src={require("./spaces/space_02.png")} alt-="space 2" />
+          <img src={require("./spaces/space_01.png")} alt="space 1" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_10.png")} alt-="space 10" />
+          <img src={require("./spaces/space_39_silph_3.png")} alt-="space 39" />
+          <img src={require("./spaces/space_38_silph_2.png")} alt-="space38" />
+          <img src={require("./spaces/space_37_silph_1.png")} alt-="space 37" />
+          <img src={require("./spaces/space_36.png")} alt-="space 36" />
+          <img src={require("./spaces/space_35.png")} alt-="space 35" />
+          <img src={require("./spaces/space_34.png")} alt-="space 34" />
+          <img src={require("./spaces/space_33_gym_4.png")} alt-="space 33" />
+          <img src={require("./spaces/space_32.png")} alt-="space 32" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_11.png")} alt-="space 11" />
+          <img src={require("./spaces/space_40_silph_4.png")} alt-="space 40" />
+          <img src={require("./spaces/space_61.png")} alt-="space 61" />
+          <img src={require("./spaces/space_60.png")} alt-="space 60" />
+          <img src={require("./spaces/space_59_gym_7.png")} alt-="space 59" />
+          <img src={require("./spaces/space_58.png")} alt-="space 58" />
+          <img src={require("./spaces/space_57.png")} alt-="space 57" />
+          <img src={require("./spaces/space_56.png")} alt-="space 56" />
+          <img src={require("./spaces/space_31.png")} alt-="space 31" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_12.png")} alt-="space 12" />
+          <img src={require("./spaces/space_41_silph_5.png")} alt-="space 41" />
+          <img src={require("./spaces/space_62.png")} alt-="space 62" />
+          <img src={require("./spaces/space_75_intro_3.png")} alt-="space 75" />
+          <img src={require("./spaces/space_74_intro_2.png")} alt-="space 74" />
+          <img src={require("./spaces/space_73_intro_1.png")} alt-="space 73" />
+          <img src={require("./spaces/space_72_champ.png")} alt-="space 72" />
+          <img src={require("./spaces/space_55.png")} alt-="space 55" />
+          <img src={require("./spaces/space_30.png")} alt-="space 30" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_13.png")} alt-="space 13" />
+          <img src={require("./spaces/space_42.png")} alt-="space 42" />
+          <img src={require("./spaces/space_63.png")} alt-="space 63" />
+          <img src={require("./spaces/space_76_intro_4.png")} alt-="space 76" />
+          <img src={require("./spaces/space_81_intro_9.png")} alt-="space 81" />
+          <img src={require("./spaces/space_80_intro_8.png")} alt-="space 80" />
+          <img src={require("./spaces/space_71_gary.png")} alt-="space 71" />
+          <img src={require("./spaces/space_54.png")} alt-="space 54" />
+          <img src={require("./spaces/space_29.png")} alt-="space 29" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_14_gym_2.png")} alt-="space 14" />
+          <img src={require("./spaces/space_43.png")} alt-="space 43" />
+          <img src={require("./spaces/space_64_gym_8.png")} alt-="space 64" />
+          <img src={require("./spaces/space_77_intro_5.png")} alt-="space 77" />
+          <img src={require("./spaces/space_78_intro_6.png")} alt-="space 78" />
+          <img src={require("./spaces/space_79_intro_7.png")} alt-="space 79" />
+          <img src={require("./spaces/space_70_four.png")} alt-="space 70" />
+          <img src={require("./spaces/space_53_gym_6.png")} alt-="space 53" />
+          <img src={require("./spaces/space_28_tower_5.png")} alt-="space 28" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_15.png")} alt-="space 15" />
+          <img src={require("./spaces/space_44_gym_5.png")} alt-="space 44" />
+          <img src={require("./spaces/space_65.png")} alt-="space 65" />
+          <img src={require("./spaces/space_66.png")} alt-="space 66" />
+          <img src={require("./spaces/space_67.png")} alt-="space 67" />
+          <img src={require("./spaces/space_68.png")} alt-="space 68" />
+          <img src={require("./spaces/space_69_birds.png")} alt-="space 69" />
+          <img
+            src={require("./spaces/space_52_safari_4.png")}
+            alt-="space 52"
+          />
+          <img src={require("./spaces/space_27_tower_4.png")} alt-="space 27" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_16.png")} alt-="space 16" />
+          <img src={require("./spaces/space_45.png")} alt-="space 45" />
+          <img src={require("./spaces/space_46.png")} alt-="space 46" />
+          <img src={require("./spaces/space_47.png")} alt-="space 47" />
+          <img src={require("./spaces/space_48.png")} alt-="space 48" />
+          <img
+            src={require("./spaces/space_49_safari_1.png")}
+            alt-="space 49"
+          />
+          <img
+            src={require("./spaces/space_50_safari_2.png")}
+            alt-="space 50"
+          />
+          <img
+            src={require("./spaces/space_51_safari_3.png")}
+            alt-="space 51"
+          />
+          <img src={require("./spaces/space_26_tower_3.png")} alt-="space 26" />
+        </div>
+        <div class="column">
+          <img src={require("./spaces/space_17.png")} alt-="space 17" />
+          <img src={require("./spaces/space_18.png")} alt-="space 18" />
+          <img src={require("./spaces/space_19.png")} alt-="space 19" />
+          <img src={require("./spaces/space_20_gym_3.png")} alt-="space 20" />
+          <img src={require("./spaces/space_21.png")} alt-="space 21" />
+          <img src={require("./spaces/space_22.png")} alt-="space 22" />
+          <img src={require("./spaces/space_23.png")} alt-="space 23" />
+          <img src={require("./spaces/space_24_tower_1.png")} alt-="space 24" />
+          <img src={require("./spaces/space_25_tower_2.png")} alt-="space 25" />
+        </div>
+      </div>
     </div>
   );
 }
