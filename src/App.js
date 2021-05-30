3ebb52164ecf5b897caddfca5e63ab7efd3d2126
Konnor <kbeaulier@wisc.edu>
2021-05-29 20:26:56 -0700

    added seo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./Styles/App.css";
 import Game from "./Game";
+import Seo from "./Components/SEO.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Seo />
         <Game />
       </div>
     );
---
+++ src/Components/SEO.js
@@ -0,0 +1,23 @@
+import Helmet from "react-helmet";
+const Seo = (props) => (      
+<Helmet>
+    <title>Pokemon Drinking Game</title>
+    <meta name="description" content="The Pokemon Drinking Board Game, play with your friends across the world with game rooms and have fun! Please drink responsibly." />
+    <meta property="og:type" content="website" />
+    <meta name="og:title" property="og:title" content="Pokemon Drinking Game" />
+    <meta name="og:description" property="og:description" content="The Pokemon Drinking Board Game, play with your friends across the world with game rooms and have fun! Please drink responsibly." />
+    <meta property="og:site_name" content="Pokemon Drinking Game" />
+    <meta property="og:url" content="http://www.pkmndrinkinggame.com" />  
+    <meta name="twitter:card" content="summary" /> 
+    <meta name="twitter:title" content="Pokemon Drinking Game" />
+    <meta name="twitter:description" content="The Pokemon Drinking Board Game, play with your friends across the world with game rooms and have fun! Please drink responsibly." />
+    <meta name="twitter:site" content="http://www.pkmndrinkinggame.com" />
+    <meta name="twitter:creator" content="Konnor Beaulier" />
+    <link rel="icon" type="image/svg" href="../logo.svg" />
+    <link rel="apple-touch-icon" href="../logo.svg" />
+    <meta property="og:image" content="../logo.svg" />  
+    <meta name="twitter:image" content="../logo.svg" />   
+    <link rel="canonical" href="http://www.pkmndrinkinggame.com" />
+</Helmet>
+)
+export default Seo;
